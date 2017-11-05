8ec0151cc313a22f91ab21e775d371054b6efebc
Doan The Anh <doantheanh172tb@gmail.com>
2017-11-05 13:36:16 +0700

    ImageGallery: saga IMAGES_LOADED

--- image-gallery/src/sagas.js
+++ image-gallery/src/sagas.js
@@ -1,6 +1,8 @@
-import { fetchImages } from './api';
+import { fetchImages } from './api'
+import { put } from 'redux-saga/effects'
 
 export function* loadImages() {
-  const images = yield fetchImages();
-  console.log("Saga::loagImages>images", images)
+  const images = yield fetchImages()
+  console.log("Saga::loagImages>put->IMAGES_LOADED", images)
+  yield put({ type: 'IMAGES_LOADED', images })
 }
